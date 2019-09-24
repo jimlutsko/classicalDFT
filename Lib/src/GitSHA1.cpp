636ab7a0ac937e50c2816876915f93da46961832
Jim Lutsko <jim@lutsko.com>
2019-09-24 12:34:12 +0200

    synch

--- Lib/src/GitSHA1.cpp
+++ Lib/src/GitSHA1.cpp
@@ -1,2 +1,2 @@
-#define GIT_SHA1 "9a08012b7a33bc663236488102742bdc8cf01ef8"
-extern const char g_GIT_SHA1[] = GIT_SHA1;
+#define GIT_SHA1 "453c2fad5f2226a661037bb2eac4769c7e277d0d"
+const char g_GIT_SHA1[] = GIT_SHA1;
