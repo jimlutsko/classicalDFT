cfcbf8418c79398bae240d9b2a61df48d8d52a5d
Jim Lutsko <jlutsko@login1.cerberus.os>
2019-09-27 12:13:40 +0200

    synch

--- Config.sh
+++ Config.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 mkdir build
 cd build 
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release $1 ..
 cd ..
 mkdir debug
 cd debug
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug $1 ..
 cd ..
---
+++ HydraScripts/Submit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+qsub -l nodes=1:ppn=20 -l walltime=1:00:00  xRun
