359c336f08d2fb1756f6500af4fe76215b0c4d6e
Jim Lutsko <jim@lutsko.com>
2019-09-30 12:23:45 +0200

    synch

--- HydraScripts/hydra_dft_make.sh
+++ HydraScripts/hydra_dft_make.sh
@@ -4,6 +4,7 @@ module purge
 # GCCcore-6.4.0 is compatible with foss-2018a
 module load  GSL/2.4-GCCcore-6.4.0
 module load  Armadillo/8.400.0-foss-2018a
+module load MathGL/2.4.1-foss-2018a
 
 ../dft_make.sh $@
 
