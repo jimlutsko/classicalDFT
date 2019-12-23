11cb75ecc4e0f24ff732a780bf55b0ba3763beae
jfl <jim@lutsko.com>
2019-12-23 11:30:14 +0100

    synch

--- Lib/include/DFT_LinAlg.h
+++ Lib/include/DFT_LinAlg.h
@@ -1,7 +1,8 @@
 #ifndef __DFT_LINALG_LUTSKO__
 #define __DFT_LINALG_LUTSKO__
 
-#include "DFT_LinAlg_EIGEN.h"
+#include "DFT_LinAlg_EIGEN_L.h"
+//#include "DFT_LinAlg_EIGEN.h"
 //#include "DFT_LinAlg_ARMA.h"
 //#include "DFT_LinAlg_ATLAS.h"
 
