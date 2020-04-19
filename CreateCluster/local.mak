c8294b1dcefde69e32b61e56058330af1a127d4f
jfl <jim@lutsko.com>
2020-04-19 16:13:55 +0200

    synch

--- CreateCluster/local.mak
+++
@@ -1,3 +0,0 @@
-PROG= createCluster
-OBS= createCluster.o
-
--- GradientDescent/droplet.d
+++
@@ -1,8 +0,0 @@
-droplet.o: droplet.cpp ../include/Grace.h ../include/options.h \
- ../include/TimeStamp.h Droplet.h ../include/Density.h \
- ../include/DFT_LinAlg.h ../include/DFT_LinAlg_ARMA.h \
- ../include/FMT_FFTW.h ../include/Display.h ../include/StringMethod.h \
- ../include/Minimizer.h ../include/Grace.h ../include/DFT.h \
- ../include/Enskog.h ../include/Potential1.h ../include/Integrator.h \
- ../include/VDW1.h ../include/Density.h ../include/FMT.h \
- ../include/FMT_Weighted_Density.h
