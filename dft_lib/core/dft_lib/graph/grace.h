fa52cd4b082870ad22370b4efb334a4135eadd51
M.A.Duran <migduroli@gmail.com>
2020-04-26 20:27:57 +0200

    doc: Add doc-strings compliant wiht Doxygen in all the methods of grace.h

--- dft_lib/core/dft_lib/graph/grace.h
+++ dft_lib/core/dft_lib/graph/grace.h
@@ -1,5 +1,5 @@
-#ifndef __CLASSICALDFT_GRACE__
-#define __CLASSICALDFT_GRACE__
+#ifndef CLASSICALDFT_GRACE_H
+#define CLASSICALDFT_GRACE_H
 
 #include <unistd.h>
 #include <grace_np.h>
@@ -7,16 +7,17 @@
 #include <string>
 #include <vector>
 
-namespace dft_core {
+namespace dft_core
+{
+  namespace grace_plot
+  {
 
-/**
-  *  @brief  UTILITY: wrapper for xmgrace graphics program.
-  *
-  *  @detailed This class provides an interface for the xmgrace graphics program which can be called programatically to provide a cheap solution  for displaying dynamically updated line graphs etc.
-  */
-  namespace grace_plot {
-
-    enum ExportFormat{
+    /**
+     * Supported file formats when saving a graph
+     *
+     * These values are used in arguments to various functions in this package.
+     */
+    enum ExportFormat {
       JPG,
       PNG,
       EPS1,
@@ -24,106 +25,102 @@ namespace dft_core {
       PDF
     };
 
+    /**
+     * Supported shapes for the data points
+     */
     enum Shape {
       CIRCLE,
       SQUARE
     };
 
-    // Default configuration values
+    /// The default X-size of the grace canvas
     const int default_x_size = 800;
+    /// The default Y-size of the grace canvas
     const int default_y_size = 600;
+
     const int default_dataset_number = 0;
+    /// The default number of graphs to be drawn on the same canvas
     const int default_number_of_graphs = 1;
+    /// The value to be used as the default minimum of an axis
     const auto min_axis_value = 0.0;
+    /// The value to be used as the default maximum of an axis
     const auto max_axis_value = 10.0;
 
-    void send_command(const std::string &cmd);
-    void error_function(const char *msg);
-    void register_grace_error_function();
-    void start_grace_communication(const double& x_size, const double& y_size, int buffer_size = 2048);
+    /**
+     * Sends a string command to the Grace CLI
+     *
+     * Helper function which sends a command string to xmgrace by using `GracePrintf` from `grace_np.h`.
+     * @param[in] cmd the command string to be run in xmgrace
+     * @throw GraceNotOpenedException when the subprocess `xmgrace` is not running, i.e. Grace is not "opened".
+     * @throw GraceCommunicationFailedException when the subprocess `xmgrace` is running but something happened inside
+     */
+    void SendCommand(const std::string &cmd);
 
-    class Grace {
-    public:
-      explicit Grace(int x_size = default_x_size, int y_size = default_y_size, int n_graph = default_number_of_graphs, bool show = true);
-      ~Grace() = default;
+    /**
+     * Parse and prints an error message from xmgrace.
+     *
+     * The library `grace_np.h` allows for registering a personalised function which will act as an error parser.
+     * Unfortunately, due to legacy reasons, such a function must deal with char* instead of modern strings.
+     * @param[in] msg the message (char*) received from `xmgrace` which will be printed out.
+     */
+    void ErrorParsingFunction(const char *msg);
 
-      bool is_initialised() { return show_; }
-      double get_x_min() { return x_min_; }
-      double get_x_max() { return x_max_; }
-      double get_y_min() { return x_min_; }
-      double get_y_max() { return x_max_; }
+    /**
+     * @brief Set up of the personalised error parsing function `ErrorParsingFunction`
+     *
+     * It uses `GraceRegisterErrorFunction` from `gnuplot_np.h` to register our personalised `ErrorParsingFunction`
+     * as the error function `xmgrace` will communicate to when an error occurs.
+     * @throw GraceException when there is an error registering the error function.
+     */
+    void RegisterGraceErrorFunction();
 
-      /*
-      void Close() { GraceClose(); }
+    /**
+     * @brief Initialisation of the communication pipe with `xmgrace`
+     *
+     * This is the first mandatory step to start the communication pipeline with `xmgrace`. This method utilises the
+     * `GraceOpenVA()` method from `grace_np.h`, which basically requires as first argument the name of the `xmgrace`
+     * executable, a `buffer_size` and a variable number of options to be passed to `xmgrace`. This method will only
+     * pass the following parameters:
+     * @param x_size the x-size of the canvas, must be positive (default = `default_x_size`)
+     * @param y_size the y-size of the canvas, must be positive (default = `default_y_size`)
+     * @param buffer_size the buffer size, must be positive (default = 2048)
+     * @throw GraceException when any of the parameters are not strictly positive
+     * @throw GraceCommunicationFailedException  when something goes wrong in the communication with the communication
+     */
+    void StartGraceCommunication(const double& x_size, const double& y_size, int buffer_size = 2048);
 
-      void Store() const;
-
-      void Store(std::string &file) const;
-
-      void Kill() const { sendCommand("Kill G0"); }
-
-      void AddPoint(double x, double y, int N = 0, int G = 0) const;
-
-      int AddDataSet(std::vector<double> const &x, std::vector<double> const &y);
-
-      void ReplaceDataSet(std::vector<double> const &x, std::vector<double> const &y, int N = 0) const;
-
-      void DeleteDataSet(int N = 0, int G = 0) const;
-
-      void SetLegend(const char *s, int n);
-
-      void SetColor(int color, int nm, int G = 0) const;
-
-      void SetXAxisLabel(const char *s, int Graph = -1);
-
-      void SetYAxisLabel(const char *s, int Graph = -1);
-
-      void SetTitle(const char *s);
-
-      void SetSubTitle(const char *s);
-
-      void Redraw(int autoScale = 1, int Graph = -1) const;
-
-      void SetLimits(double xmin, double xmax, double ymin, double ymax, int Graph = -1);
-
-      void SetTicks(double dx, double dy, int Graph = -1);
-
-      void SetSymbol(int set, int symbol, int Graph = -1);
-
-      void SetSymbolFill(int set, int color, int Graph = -1);
-
-      void Pause() const {
-        char cc;
-        std::cout << "Enter any character to continue: ";
-        std::cin >> cc;
-      }
-
-      void NoLine(int dataSet, int Graph = -1);
-
-      void Line(int dataSet, int Graph = -1);
-
-      void Circle(int dataSet, int Graph = -1);
-
-      void Square(int dataSet, int Graph = -1);
-
-      void Size(int dataSet, double size, int Graph = -1);
-
-      static int CIRCLE;
-      static int SQUARE;
-
-      void PrintToFile(std::string &file, int format);
-
-      void AutoTic(int Graph = -1) const;
+    /**
+     * @brief  Utility: Wrapper for xmgrace graphics program.
+     * @detailed This class provides an interface for the xmgrace graphics program which can be called programmatically
+     * to provide a cheap solution  for displaying dynamically updated line graphs etc.
     */
-    private:
-      double x_min_ = min_axis_value;
-      double x_max_ = max_axis_value;
-      double y_min_ = min_axis_value;
-      double y_max_ = max_axis_value;
-      int n_max_data_set_ = default_dataset_number;
-      int n_graph_ = default_number_of_graphs;
-      bool show_ = false;
+    class Grace
+    {
+      public:
+        /// Explicit constructor, which avoids implicit conversions
+        explicit Grace(int x_size = default_x_size, int y_size = default_y_size, int n_graph = default_number_of_graphs, bool show = true);
+        /// Default destructor
+        ~Grace() = default;
 
+        /// Inspector of the property `show_` which governs the construction of a Grace object in case of being `false`
+        const bool& is_initialised() const { return show_; }
+        /// The minimum value for the X axis
+        const double& x_min() const { return x_min_; }
+        /// The maximum value for the X axis
+        const double& x_max() const { return x_max_; }
+        /// The minimum value for the Y axis
+        const double& y_min() const { return x_min_; }
+        /// The maximum value for the Y axis
+        const double& y_max() const { return x_max_; }
+
+      private:
+        double x_min_ = min_axis_value;
+        double x_max_ = max_axis_value;
+        double y_min_ = min_axis_value;
+        double y_max_ = max_axis_value;
+        int n_max_data_set_ = default_dataset_number;
+        int n_graph_ = default_number_of_graphs;
+        bool show_ = false;
     };
   }
 }
