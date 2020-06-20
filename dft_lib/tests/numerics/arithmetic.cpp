3dc0dad4c3bc6adb03dc3a549916850f8a764a6e
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2020-06-20 18:53:10 +0200

    #23 feat: add accuracy test to Kahan-Babuska method

--- dft_lib/tests/numerics/arithmetic.cpp
+++ dft_lib/tests/numerics/arithmetic.cpp
@@ -18,5 +18,17 @@ TEST(numerics, kahan_babuska_sum_works_ok)
   EXPECT_LE(std::abs(std::abs(expected)-std::abs(actual)), std::abs(err));
 }
 
+TEST(numerics, kahan_babuska_accuracy_ok)
+{
+  auto x_input = std::vector<double>{1.0, 2.0, 4.0, 3.0};
+
+  int expected = 10;
+  double actual, err;
+  std::tie(actual, err) = arithmetic::summation::KahanBabuskaSum(x_input);
+  std::cout << err << std::endl;
+  std::cout << DBL_EPSILON << std::endl;
+  std::cout << 2*DBL_EPSILON*expected << std::endl;
+  EXPECT_LE(std::abs(err), 2*DBL_EPSILON*expected);
+}
 
 //endregion
