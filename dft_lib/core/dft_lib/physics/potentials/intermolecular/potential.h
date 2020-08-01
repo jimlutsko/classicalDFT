c812fe3f38ca09bad5fa27fbce9c9aa6aa434099
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2020-08-02 00:42:09 +0200

    #24 feat: Add Potential skeleton with fundamental attributes and methods

--- dft_lib/core/dft_lib/physics/potentials/intermolecular/potential.h
+++ dft_lib/core/dft_lib/physics/potentials/intermolecular/potential.h
@@ -1,27 +1,92 @@
 #ifndef CLASSICALDFT_POTENTIAL_H
 #define CLASSICALDFT_POTENTIAL_H
 
+#include <iostream>
+
 namespace dft_core
 {
 namespace physics
 {
 namespace potentials
 {
-namespace
+
+const double DEFAULT_LENGTH_SCALE = 1.0;
+const double DEFAULT_ENERGY_SCALE = 1.0;
+const double DEFAULT_ZERO = 0.0;
+
+namespace intermolecular
 {
 
+enum class PotentialName
+{
+  LennardJones = 0,
+  tenWoldeFrenkel,
+  WangRamirezDobnikarFrenkel
+};
+
+/**
+ * @brief Potential-energy general model: Base for all the interaction potentials.
+ * The potential class brings the ability to split the potential contribution into two parts:
+ * - hard sphere and,
+ * - attractive,
+ * and, also comes with the functionality to compute the hard-sphere diameter.
+ */
 class Potential
 {
-  // region Attributes:
-  // endregion
+ private:
 
+  // region Attributes:
+
+  /// The potential length scale
+  double sigma_ = DEFAULT_ENERGY_SCALE;
+  /// The potential energy scale
+  double epsilon_ = DEFAULT_ENERGY_SCALE;
+  /// The amount of energy the potential will be shifted
+  double eps_shift_ = DEFAULT_ZERO;
+  /// The cut-off distance, as of which the potential will be truncated
+  double r_cutoff_ = -DEFAULT_LENGTH_SCALE;
+  /// The position where the potential reaches its mimimum
+  double r_min_ = DEFAULT_LENGTH_SCALE;
+  /// The minimum of the potential energy
+  double v_min_ = -DEFAULT_ENERGY_SCALE;
+  /// Weeks-Chandler-Andersen continuation limit
+  double r_attractive_min_ = DEFAULT_ZERO;
+  /// The position at which potential goes to zero
+  double r_zero_ = DEFAULT_LENGTH_SCALE;
+  /// Boolean variable to indicate whether Barker-Henderson model (repulsive-attractive split) is applied
+  bool bh_perturbation_ = false;
+  /// The non-dimensional energy (used to pass the temperature during evaluation of numerical integrals)
+  double kT_ = DEFAULT_ENERGY_SCALE;
+  /// The abbreviation of the intermolecular potential name
+  PotentialName potential_id_;
+
+  // endregion
+ public:
   // region Cttor:
+  Potential() = default;
+  Potential(double sigma, double epsilon, double r_cutoff);
+
   // endregion
 
   // region Inspectors:
+  double sigma() const;
+  double epsilon() const;
+  double eps_shift() const;
+  double r_cutoff() const;
+  double r_min() const;
+  double v_min() const;
+  double r_attractive_min() const;
+  double r_zero() const;
+  bool bh_perturbation() const;
+  double kT() const;
+
+  const PotentialName& id() const;
+  std::string identifier() const;
   // endregion
 
   // region Mutators:
+  /// Computes (and sets) the hard-core diameter of the original potential (e.g. zero for LJ)
+  virtual double getHardCoreDiameter() const = 0;
   // endregion
 };
 
--- dft_lib/core/src/physics/potentials/intermolecular/potential.cpp
+++ dft_lib/core/src/physics/potentials/intermolecular/potential.cpp
@@ -1,1 +1,57 @@
 #include "dft_lib/physics/potentials/intermolecular/potential.h"
+
+
+namespace dft_core
+{
+namespace physics
+{
+namespace potentials
+{
+namespace intermolecular {
+
+Potential::Potential(double sigma, double epsilon, double r_cutoff)
+    : sigma_(sigma),
+      epsilon_(epsilon),
+      r_cutoff_(r_cutoff),
+      bh_perturbation_(false),
+      eps_shift_(DEFAULT_ZERO),
+      r_min_(DEFAULT_ZERO),
+      v_min_(DEFAULT_ZERO),
+      r_attractive_min_(DEFAULT_ZERO),
+      r_zero_(DEFAULT_LENGTH_SCALE),
+      kT_(DEFAULT_ENERGY_SCALE) {}
+
+double Potential::sigma() const { return sigma_; }
+double Potential::epsilon() const { return epsilon_; }
+double Potential::eps_shift() const { return eps_shift_; }
+double Potential::r_cutoff() const { return r_cutoff_; }
+double Potential::r_min() const { return r_min_; }
+double Potential::v_min() const { return v_min_; }
+double Potential::r_attractive_min() const { return r_attractive_min_; }
+double Potential::r_zero() const { return r_zero_; }
+bool Potential::bh_perturbation() const { return bh_perturbation_; }
+double Potential::kT() const { return kT_; }
+const PotentialName& Potential::id() const { return potential_id_; }
+
+std::string Potential::identifier() const {
+  std::string name;
+  switch (this->id()) {
+    case PotentialName::LennardJones:
+      name = "LennardJones";
+      break;
+    case PotentialName::tenWoldeFrenkel:
+      name = "tenWoldeFrenkel";
+      break;
+    case PotentialName::WangRamirezDobnikarFrenkel:
+      name = "WangRamirezDobnikarFrenkel";
+      break;
+    default:
+      name = "LennardJones";
+  }
+
+  return name + "_" + std::to_string(this->sigma()) + "_" + std::to_string(this->epsilon()) + "_" +
+         std::to_string(this->r_cutoff()) + "_" + std::to_string(this->r_attractive_min()) + "_" +
+         std::to_string(this->bh_perturbation());
+}
+
+}}}}
