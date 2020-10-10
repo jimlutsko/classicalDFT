68bd466cf58ea68b130ec476533fe34e42231f3e
Miguel A. Duran-Olivencia <migduroli@gmail.com>
2020-10-10 16:46:41 +0200

    #29 init: Add point.h to start with the refactoring of Lattice.h

---
+++ dft_lib/core/dft_lib/geometry/point.h
@@ -0,0 +1,17 @@
+#ifndef CLASSICALDFT_POINT_H
+#define CLASSICALDFT_POINT_H
+
+namespace dft_core {
+namespace geometry {
+
+class Point
+{
+ private:
+ protected:
+ public:
+  Point() = default;
+};
+
+}}
+
+#endif  // CLASSICALDFT_POINT_H
