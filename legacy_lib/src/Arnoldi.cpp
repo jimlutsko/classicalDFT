2088145d5ed588de37180daf88ba6f8724b1bee9
James Lutsko <jim@lutsko.com>
2022-08-15 10:42:45 +0200

    synch

--- legacy_lib/src/Arnoldi.cpp
+++ legacy_lib/src/Arnoldi.cpp
@@ -71,7 +71,6 @@ void Arnoldi::matrix_dot_v(arma::cx_vec v, arma::cx_vec& d2F, double shift) cons
 
 double Arnoldi::determine_largest_eigenvalue(vector<DFT_FFT> &eigen_vector, double shift, string Filename, int k, int p, long maxSteps, double tol) const
 {
-<<<<<<< HEAD
 	if(verbose_) cout << endl;
 	if(verbose_) cout << myColor::YELLOW;
 	if(verbose_) cout << "\tFixed boundary = " << matrix_.is_fixed_boundary() << ", MaxIterations = " << maxSteps << ", tolerence = " << tol << endl;
@@ -103,134 +102,106 @@ double Arnoldi::determine_largest_eigenvalue(vector<DFT_FFT> &eigen_vector, doub
 	const int Ny = matrix_.get_dimension(1);
 	const int Nz = matrix_.get_dimension(2);
 	const long Ntot = Nx*Ny*Nz;	
-=======
-  if(verbose_) cout << endl;
-  if(verbose_) cout << myColor::YELLOW;
-  if(verbose_) cout << "\tFixed boundary = " << matrix_.is_fixed_boundary() << ", MaxIterations = " << maxSteps << ", tolerence = " << tol << endl;
-  if(verbose_) cout << myColor::RESET;
-  if(verbose_) cout << endl;
 	
-  int sysres = system("zip -r -q arnoldi_backup.zip arnoldi/ eigenvectors/");
-  sysres = system("rm -r arnoldi");
-  sysres = system("mkdir arnoldi");
-  sysres = system("rm -r eigenvectors");
-  sysres = system("mkdir eigenvectors");
->>>>>>> 2fe278c4e604a7eb887e966dc34dddb9df53fbdd
+	// Pass negative tolerance to tell the algorithm it must keep iterating
+	bool dont_stop_iterating = false;
+	if (tol<0) {dont_stop_iterating = true; tol = abs(tol);}
 	
-  ofstream ofile_iter("arnoldi/iterations.dat");
-  ofile_iter << "# Largest eigenvalues from Implicitely Restarted Arnoldi method" << endl;
-  ofile_iter << "# " << endl;
-  ofile_iter << "# These are Ritz values (real and imaginary components) and Ritz" << endl;
-  ofile_iter << "# estimates for the errors. In other words these are Rayleigh quotients" << endl;
-  ofile_iter << "# using the best approximations of the associated eigenvectors and the" << endl;
-  ofile_iter << "# error is equivalent to the residual |Av-xv|." << endl;
-  ofile_iter << "# " << endl;
-  ofile_iter << "#" << setw(7) << "iter*p" << setw(16) <<  "real"  << setw(16) <<  "imag"  << setw(16) << "error" << endl;
+	arma::cx_mat Vk,Hk;
+	arma::cx_vec fk;
 	
-  const int species = 0;
-  const int Nx = matrix_.get_dimension(0);
-  const int Ny = matrix_.get_dimension(1);
-  const int Nz = matrix_.get_dimension(2);
-  const long Ntot = Nx*Ny*Nz;	
+	extend_arnoldi_factorisation(Vk, Hk, fk, 0, k, shift, tol);
 	
-  // Pass negative tolerance to tell the algorithm it must keep iterating
-  bool dont_stop_iterating = false;
-  if (tol<0) {dont_stop_iterating = true; tol = abs(tol);}
+	arma::cx_vec Hk_eigval;
+	arma::cx_mat Hk_eigvec;
 	
-  arma::cx_mat Vk,Hk;
-  arma::cx_vec fk;
+	compute_and_sort_eigenvectors(Hk, Hk_eigvec, Hk_eigval);
 	
-  extend_arnoldi_factorisation(Vk, Hk, fk, 0, k, shift, tol);
+	// Iterate
+	int iter = 0;
+	bool converged = false;
+	double eigen_value_old = 0.0;
 	
-  arma::cx_vec Hk_eigval;
-  arma::cx_mat Hk_eigvec;
+	arma::cx_vec eigval;
+	arma::cx_mat eigvec;
 	
-  compute_and_sort_eigenvectors(Hk, Hk_eigvec, Hk_eigval);
-	
-  // Iterate
-  int iter = 0;
-  bool converged = false;
-  double eigen_value_old = 0.0;
-	
-  arma::cx_vec eigval;
-  arma::cx_mat eigvec;
-	
-  while (!converged || dont_stop_iterating)
-    {
-      // p-step extension of Arnoldi factorisation
-		
-      arma::cx_mat Vp = Vk;
-      arma::cx_mat Hp = Hk;
-      arma::cx_vec fp = fk;
-		
-      extend_arnoldi_factorisation(Vp, Hp, fp, k, p, shift, tol);
-		
-      arma::cx_vec Hp_eigval;
-      arma::cx_mat Hp_eigvec;
-		
-      compute_and_sort_eigenvectors(Hp, Hp_eigvec, Hp_eigval);
-		
-      // Sort eigenvalues for extended H-matrix
-		
-      arma::cx_vec Hk_eigval(k);
-      arma::cx_vec Hp_eigval_discard(p);
-		
-      for (int i=0; i<k; i++) Hk_eigval[i] = Hp_eigval[i];
-      for (int i=k; i<k+p; i++) Hp_eigval_discard[i] = Hp_eigval[i];
-		
-      // Filter out unwanted eigenvalues
-		
-      arma::cx_vec q(k+p); q.zeros(); q(k+p-1) = 1;
-		
-      for (int i=0; i<p; i++)
+	while (!converged || dont_stop_iterating)
 	{
-	  arma::cx_mat Hp_shifted = Hp;
-	  for (int j=0; j<k+p; j++) Hp_shifted(j,j) -= Hp_eigval_discard[i];
+		// p-step extension of Arnoldi factorisation
+		
+		arma::cx_mat Vp = Vk;
+		arma::cx_mat Hp = Hk;
+		arma::cx_vec fp = fk;
+		
+		extend_arnoldi_factorisation(Vp, Hp, fp, k, p, shift, tol);
+		
+		arma::cx_vec Hp_eigval;
+		arma::cx_mat Hp_eigvec;
+		
+		compute_and_sort_eigenvectors(Hp, Hp_eigvec, Hp_eigval);
+		
+		// Sort eigenvalues for extended H-matrix
+		
+		arma::cx_vec Hk_eigval(k);
+		arma::cx_vec Hp_eigval_discard(p);
+		
+		for (int i=0; i<k; i++) Hk_eigval[i] = Hp_eigval[i];
+		for (int i=k; i<k+p; i++) Hp_eigval_discard[i] = Hp_eigval[i];
+		
+		// Filter out unwanted eigenvalues
+		
+		arma::cx_vec q(k+p); q.zeros(); q(k+p-1) = 1;
+		
+		for (int i=0; i<p; i++)
+		{
+			arma::cx_mat Hp_shifted = Hp;
+			for (int j=0; j<k+p; j++) Hp_shifted(j,j) -= Hp_eigval_discard[i];
 			
-	  arma::cx_mat Q,R;
-	  arma::qr(Q,R,Hp_shifted);
+			arma::cx_mat Q,R;
+			arma::qr(Q,R,Hp_shifted);
 			
-	  Hp = Q.t()*Hp*Q;
-	  Vp = Vp*Q;
+			Hp = Q.t()*Hp*Q;
+			Vp = Vp*Q;
 			
-	  Hp_eigvec = Q.t()*Hp_eigvec; // Missing information in Radke's thesis
+			Hp_eigvec = Q.t()*Hp_eigvec; // Missing information in Radke's thesis
 			
-	  q = Q.t()*q; // Not the original line: misktake in Radke's thesis
-	}
+			q = Q.t()*q; // Not the original line: misktake in Radke's thesis
+		}
 		
-      // Update Arnoldi factorisation
-      long nelem = Vk.n_rows;
-      for (long i=0; i<nelem; i++) for (int j=0; j<k; j++) Vk(i,j) = Vp(i,j);
-      for (int i=0; i<k; i++) for (int j=0; j<k; j++) Hk(i,j) = Hp(i,j);
-      for (int i=0; i<k; i++) for (int j=0; j<k; j++) Hk_eigvec(j,i) = Hp_eigvec(j,i);
+		// Update Arnoldi factorisation
+		long nelem = Vk.n_rows;
+		for (long i=0; i<nelem; i++) for (int j=0; j<k; j++) Vk(i,j) = Vp(i,j);
+		for (int i=0; i<k; i++) for (int j=0; j<k; j++) Hk(i,j) = Hp(i,j);
+		for (int i=0; i<k; i++) for (int j=0; j<k; j++) Hk_eigvec(j,i) = Hp_eigvec(j,i);
 		
-      arma::cx_vec vv(nelem); for (long i=0; i<nelem; i++) vv[i] = Vp(i,k);
-      fk = vv*Hp(k,k-1) + fp*q(k-1);
+		arma::cx_vec vv(nelem); for (long i=0; i<nelem; i++) vv[i] = Vp(i,k);
+		fk = vv*Hp(k,k-1) + fp*q(k-1);
 		
-      save_Arnoldi_matrices(Vk,Hk);
+		save_Arnoldi_matrices(Vk,Hk);
 		
-      if (!check_factorisation(Vk,Hk,fk,shift,tol))
-	throw runtime_error("(IRA) Arnoldi factorisation does not check out");
+		if (!check_factorisation(Vk,Hk,fk,shift,tol))
+			throw runtime_error("(IRA) Arnoldi factorisation does not check out");
 		
-      // Update approximations for eigenvalues/vectors
+		// Update approximations for eigenvalues/vectors
 		
-      eigval = Hk_eigval;
-      eigvec = arma::cx_mat(nelem,k);
+		eigval = Hk_eigval;
+		eigvec = arma::cx_mat(nelem,k);
 		
-      for (int i=0; i<k; i++)
-	{
-	  arma::cx_vec wi(k);
-	  for (int j=0; j<k; j++) wi[j] = Hk_eigvec(j,i);
+		for (int i=0; i<k; i++)
+		{
+			arma::cx_vec wi(k);
+			for (int j=0; j<k; j++) wi[j] = Hk_eigvec(j,i);
 			
-	  arma::cx_vec vi = Vk*wi;
-	  for (long j=0; j<nelem; j++) eigvec(j,i) = vi[j];
-	}
+			arma::cx_vec vi = Vk*wi;
+ 			for (long j=0; j<nelem; j++) eigvec(j,i) = vi[j];
+		}
 		
-      // TODO: They are not exactly the same, why??
-      // Note: For testing only -- The norm of the residuals are the same as the Ritz estimates
-      check_eigenvectors(eigvec,eigval,shift,tol);
+		// TODO: They are not exactly the same, why??
+		// Note: For testing only -- The norm of the residuals are the same as the Ritz estimates
+		check_eigenvectors(eigvec,eigval,shift,tol);
 		
-<<<<<<< HEAD
+		// Convergence checks
+		
 		iter++;
 		//JFL
 		//if (iter>=maxSteps) converged = true; //throw runtime_error("IRArnoldi method: Exceeded max iterations");		
@@ -242,28 +213,14 @@ double Arnoldi::determine_largest_eigenvalue(vector<DFT_FFT> &eigen_vector, doub
 		for (int i=0; i<k; i++)
 		{
 			double ritz_estimate = abs(Hk_eigvec(k-1,i))*norm(fk);
-=======
-      // Convergence checks
-		
-      iter++;
-      //JFL
-      //		if (iter>=maxSteps) throw runtime_error("IRArnoldi method: Exceeded max iterations");
-      //		converged = true;
-      if (iter>=maxSteps) 	return eigval[0].real()-shift;
-      
-      for (int i=0; i<k; i++)
-	{
-	  double ritz_estimate = abs(Hk_eigvec(k-1,i))*norm(fk);
->>>>>>> 2fe278c4e604a7eb887e966dc34dddb9df53fbdd
 			
-	  // Not converged if (large ritz estimate) and 
-	  // (eigenvalue not degenerated with the largest discarded value)
-	  // The second condition is necessary because if the last eigenvalue
-	  // is degenerate with another that is discarded, we never complete
-	  // the calculation of the corresponding vector subspace since we
-	  // never record all necessary Arnoldi vectors to contruct the basis.
+			// Not converged if (large ritz estimate) and 
+			// (eigenvalue not degenerated with the largest discarded value)
+			// The second condition is necessary because if the last eigenvalue
+			// is degenerate with another that is discarded, we never complete
+			// the calculation of the corresponding vector subspace since we
+			// never record all necessary Arnoldi vectors to contruct the basis.
 			
-<<<<<<< HEAD
 			if ( ritz_estimate > tol*abs(eigval[i]) &&
 			     abs(eigval[i]-Hp_eigval_discard[0]) > tol) 
 			{
@@ -277,30 +234,17 @@ double Arnoldi::determine_largest_eigenvalue(vector<DFT_FFT> &eigen_vector, doub
 			  }
 		}
 		if(debug_) ofile_iter << endl;
-=======
-	  if ( ritz_estimate > tol*abs(eigval[i]) &&
-	       abs(eigval[i]-Hp_eigval_discard[0]) > tol) 
-	    {
-	      converged = false;
-	    }
+		
+		// Save leading eigenvectors
+		// JFL: This code needs to be replaced by something that only uses native (Armadillo) functions
+		
+		eigen_vector[species].initialize(Nx,Ny,Nz);
+		
+		for (int i=k-1; i>=0; i--)
+		{
+			for(long j=0; j<Ntot; j++)
+				eigen_vector[species].Real().set(j,eigvec(j,i).real());
 			
-	  ofile_iter << scientific << setprecision(6);
-	  ofile_iter << setw(8) << iter*p << setw(16) << eigval[i].real()-shift << setw(16) << eigval[i].imag() << setw(16) << ritz_estimate << endl;
-	}
-      ofile_iter << endl;
->>>>>>> 2fe278c4e604a7eb887e966dc34dddb9df53fbdd
-		
-      // Save leading eigenvectors
-      // JFL: This code needs to be replaced by something that only uses native (Armadillo) functions
-		
-      eigen_vector[species].initialize(Nx,Ny,Nz);
-		
-      for (int i=k-1; i>=0; i--)
-	{
-	  for(long j=0; j<Ntot; j++)
-	    eigen_vector[species].Real().set(j,eigvec(j,i).real());
-			
-<<<<<<< HEAD
 			eigen_vector[species].Real().normalise();
 			eigen_vector[species].do_real_2_fourier();
 
@@ -311,19 +255,9 @@ double Arnoldi::determine_largest_eigenvalue(vector<DFT_FFT> &eigen_vector, doub
 			    of.close();
 			  }
 		}
-=======
-	  eigen_vector[species].Real().normalise();
-	  eigen_vector[species].do_real_2_fourier();
-			
-	  ofstream of("eigenvectors/density_eigenvector_"+to_string(i)+".dat");
-	  of << eigen_vector[species].Real();
-	  of.close();
-	}
->>>>>>> 2fe278c4e604a7eb887e966dc34dddb9df53fbdd
 	       
-      // Report in terminal
+		// Report in terminal
 		
-<<<<<<< HEAD
 		cout << myColor::YELLOW;
 		cout << setprecision(6);
 		cout << '\r'; cout << "\t" << "iteration = " << iter << " shift = " << shift << " eigen_value = " << setw(12) << eigval[0].real()-shift;
@@ -338,31 +272,17 @@ double Arnoldi::determine_largest_eigenvalue(vector<DFT_FFT> &eigen_vector, doub
 		eigen_value_old = eigval[0].real()-shift;
 	}
 	cout << endl;
-=======
-      cout << myColor::YELLOW;
-      cout << setprecision(6);
-      cout << '\r'; cout << "\t" << "iteration = " << iter << " shift = " << shift << " eigen_value = " << setw(12) << eigval[0].real()-shift;
-      cout << myColor::RESET;
-		
-      ofstream debug("debug.dat", (iter == 0 ? ios::trunc : ios::app));
-      debug << iter << " " << eigval[0].real()-shift << " " << fabs(eigval[0].real()-shift - eigen_value_old)/fabs(eigval[0].real()-shift) << " " << fabs(eigval[0].real()-shift - eigen_value_old) << " " << abs(Hk_eigvec(k-1,0))*norm(fk) << endl;
-      debug.close();
-		
-      eigen_value_old = eigval[0].real()-shift;
-    }
-  cout << endl;
->>>>>>> 2fe278c4e604a7eb887e966dc34dddb9df53fbdd
 	
-  // This is not already initialized ???
-  eigen_vector[species].initialize(Nx,Ny,Nz);		
-  for(long j=0; j<Ntot; j++)
-    eigen_vector[species].Real().set(j,eigvec(j,0).real()); 	    
-  eigen_vector[species].Real().normalise();
-  eigen_vector[species].do_real_2_fourier();
+	// This is not already initialized ???
+	eigen_vector[species].initialize(Nx,Ny,Nz);		
+	for(long j=0; j<Ntot; j++)
+	  eigen_vector[species].Real().set(j,eigvec(j,0).real()); 	    
+	eigen_vector[species].Real().normalise();
+	eigen_vector[species].do_real_2_fourier();
 	
-  if(verbose_) cout << endl;
+	if(verbose_) cout << endl;
 	
-  return eigval[0].real()-shift;
+	return eigval[0].real()-shift;
 }
 
 void compute_and_sort_eigenvectors(arma::cx_mat H, arma::cx_mat &eigvec, arma::cx_vec &eigval)
